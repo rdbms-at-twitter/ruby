1bc9f42f33be5ec13252df89c1f099d6e51497b7
EC2 Default User <ec2-user@ip-172-30-2-137.ap-northeast-1.compute.internal>
2019-08-13 11:42:38 +0000

    nil check and boolean

---
+++ boolean_check.rb
@@ -0,0 +1,9 @@
+#irb(main):001:0> require './boolean_check.rb'
+#Value is Same
+#=> true
+#irb(main):002:0>
+
+value = 100
+if value == 100
+  puts "Value is Same"
+end
---
+++ nil_check.rb
@@ -0,0 +1,6 @@
+#irb(main):001:0> require './nil_check.rb'
+#=> true
+#irb(main):002:0>
+
+value = nil
+value.nil?

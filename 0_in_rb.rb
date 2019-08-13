e85badd253b80235a9865be00140d846b36bd4e6
EC2 Default User <ec2-user@ip-172-30-2-137.ap-northeast-1.compute.internal>
2019-08-13 12:09:39 +0000

    0 is true and else func check

---
+++ 0_in_rb.rb
@@ -0,0 +1,9 @@
+#irb(main):001:0> require './0_in_rb.rb'
+#In ruby 0 is not same as nil and false
+#=> true
+#irb(main):002:0>
+
+value = 0
+if value
+ puts "In ruby 0 is not same as nil and false"
+end
---
+++ boolean_else.rb
@@ -0,0 +1,11 @@
+#irb(main):001:0> require './boolean_else.rb'
+#this is 100
+#=> true
+#irb(main):002:0> 
+
+value = 100
+if value == 100 then
+    puts "this is 100"
+else 
+    puts "this is not 100"
+end
